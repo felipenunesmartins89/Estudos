0990ac13d39b39d44d6045cfacb604534f99378a
felipe <felipenunesmartins89@gmail.com>
2019-06-25 21:16:22 -0300

    commit

---
+++ conceitos_funções.py
@@ -0,0 +1,27 @@
+
+
+"""
+Funções nomeadas (Def)
+"""
+
+ def soma(x, y):
+      return x + y
+
+
+"""
+Funções Anônimas (Lambda)
+"""
+
+anonima_soma = lambda x, u: x + y
+
+"""
+Funções de Classe (Class)
+"""
+
+class classe_soma:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+        def __call_(self):
+            return self.x + self. y
