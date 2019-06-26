145a4c6260283c27d4720c3548adbbe4a6f90a72
felipe <felipenunesmartins89@gmail.com>
2019-06-25 23:01:20 -0300

    commit

--- funções_nomeadas.py
+++ funções_nomeadas.py
@@ -1,27 +1,80 @@
-def soma(x, y):
-    """
-    Função que soma dois valores
-    """
-    return x + y
+# def soma(x, y):
+#     """
+#     Função que soma dois valores
+#     """
+#     return x + y
+#
+#
+# print(soma.__name__) #retorna o nome da função
+#
+# print(dir(soma)) #retorna métodos e atributos dentro do objeto do tipo função
+#
+# print(soma.__doc__) #retorna o que tem dentro da nossa função(metadados)
+#
+# print(soma (2, 2)) #chama função soma e passa 2 parametros
+#
+#
+# def div(x, y):
+#     """
+#     Função que soma dois valores
+#     """
+#     return x / y
+#
+# try:
+#     print(div(0, 0)) #nenhum número é divisivel por zero
+# except:
+#     print(div.__name__) #retornou o nome da função
+#
+#
+# #"""
+# #EXEMPLO DE FUNÇÃO NOMEADA PARA RETORNAR MAIORES DE 27
+# #"""
+#
+# users = [
+#   {
+#     "id": 1,
+#     "name": "Allan",
+#     "age": 27,
+#     "profile_picture": "http://…",
+#     "city": "São Paulo"
+#   },
+#   {
+#     "id": 2,
+#     "name": "Julie",
+#     "age": 29,
+#     "profile_picture": "http://…",
+#     "city": "Curitiba"
+#   },
+#   {
+#     "id": 3,
+#     "name": "Pedro",
+#     "age": 31,
+#     "profile_picture": "http://…",
+#     "city": "Rio de Janeiro"
+#   }
+# ]
+#
+#
+# # Crie a função que irá filtrar a lista
+# def maior_27(pessoas):
+#   return pessoas["age"] > 27
+#
+# # Filtramos os usuários utilizando o método filter
+# filtered_users = filter(maior_27, users)
+#
+# print(list(filtered_users))
+#
 
 
-print(soma.__name__) #retorna o nome da função
+def externa(id):
+    dic = {'pt': 'Olá', 'pi': 'Ahoy', 'en': 'hello'}
 
-print(dir(soma)) #retorna métodos e atributos dentro do objeto do tipo função
+    def interna(nome):
+        print(f'{dic[id]} {nome}')
+    return interna
 
-print(soma.__doc__) #retorna o que tem dentro da nossa função(metadados)
-
-print(soma (2, 2)) #chama função soma e passa 2 parametros
-
-
-def div(x, y):
-    """
-    Função que soma dois valores
-    """
-    return x / y
-
-try:
-    print(div(0, 0)) #nenhum número é divisivel por zero
-except:
-    print(div.__name__) #retornou o nome da função
+func = externa('en')
+func('Pedro')
+func('Julio')
+func('Marivaldo')
 
